6d11e4f69967f1d41019897851a90603ed0c9103
John Cole <johncolejr9@gmail.com>
2020-12-17 14:46:01 -0500

    fixed backend and added .env

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, redirect, flash, request
 from mysqlconnection import connectToMySQL
+import dotenv
 import re
+import sys
+import os
+
 
 app = Flask(__name__)
-app.secret_key="point"
+app.SECRET_KEY = os.getenv('APP_SECRET_KEY')
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @app.route('/contact-form', methods=["POST"])
 def add_contact_to_database():
@@ -17,9 +21,9 @@ def add_contact_to_database():
     if len(request.form['name']) < 1:
         is_valid = False
         flash("Name is a required field")
-    if not request.form['fn'].isalpha():
+    if not request.form['name'].isalpha():
         is_valid = False
-        flash("First name can only contain alphabetic chracters")
+        flash("Name can only contain alphabetic chracters")
     if not EMAIL_REGEX.match(request.form['email']):
         is_valid = False
         flash("Invalid email address!")
@@ -28,7 +32,7 @@ def add_contact_to_database():
         flash ("Must contain message")
 
     if is_valid:
-        query = "INSERT INTO contact-form (name, email, message, created_at, updated_at) VALUES (%(n)s, %(em)s, %(m)s, NOW(), NOW());"
+        query = "INSERT INTO contact (name, email, message, created_at, updated_at) VALUES (%(n)s, %(em)s, %(m)s, NOW(), NOW());"
         data = {
             'n': request.form['name'],
             'em': request.form['email'],
@@ -37,8 +41,8 @@ def add_contact_to_database():
         db = connectToMySQL('devspoint')
         db.query_db(query, data)
         flash("Your message has been sent")
-    
-    return redirect("/")
+
+    return redirect("/#contact")
 
 if __name__ == "__main__":
     app.run(debug=True)
